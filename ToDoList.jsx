9002585fdda7686d8a16891939758b59801f1cbd
Daniel <daniel.barbieri@edu.sait.ca>
2023-11-09 12:58:55 -0700

    Lab 3
    Lab 3 Commit

--- ToDoList.jsx
+++ ToDoList.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
- 
-const ToDoList = () => {
+import { View, Text, StyleSheet } from 'react-native';
+
+const Task = ({ taskText }) => {
+  return <Text>{taskText}</Text>;
+};
+
+const ToDoList = ({ tasks }) => {
   return (
-<ScrollView style={styles.listContainer}>
-<Text style={styles.task}>Task 1: Example Task</Text>
-<Text style={styles.task}>Task 2: Another Example Task</Text>
-<Text style={styles.task}>Task 3: Yet Another Example Task</Text>
-      {/* Add more tasks as needed */}
-</ScrollView>
+    <View>
+      {tasks.map((task, index) => (
+        <Task key={index} taskText={task} />
+      ))}
+    </View>
   );
 };
- 
-const styles = StyleSheet.create({
-  listContainer: {
-    margin: 16,
-    padding: 8,
-    borderColor: 'grey',
-    borderWidth: 1,
-    borderRadius: 8,
-  },
-  task: {
-    fontSize: 16,
-    marginVertical: 8,
-  },
-});
- 
+
 export default ToDoList;
